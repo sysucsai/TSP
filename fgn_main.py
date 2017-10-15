5510bcfa38431686a53bbdd44de3ae9ef6d3c016
408534734 <408534734@qq.com>
2017-10-15 11:50:05 +0800

    Update fgn_main.cpp and devlopment read_ans.py which can read and print the standard ans automatically.

--- fgn_main.py
+++ fgn_main.py
@@ -1,75 +1,119 @@
 import readin
 import show_path
 import random
+import math
+import read_ans
 
 n = -1
-map = ()
+dis = []
 
 
-def dis(i,j):
-	return ((map[i][0]-map[j][0])**2 + (map[i][1]-map[j][1])**2) ** 0.5
-
+def inital_dis(n, map):
+	global dis
+	for i in range(n):
+		dis.append([])
+		for j in range(n):
+			dis[i].append(((map[i][0]-map[j][0])**2 + (map[i][1]-map[j][1])**2) ** 0.5)
+		dis[i] = tuple(dis[i])
+	dis = tuple(dis)
 
 def dis_cal(path):
 	back = 0
 	for i in range(n):
-		back += dis(path[i], path[(i+1)%n])
+		back += dis[path[i]][path[(i+1)%n]]
 	return back
 
 
 def swap(old_ans, old_path, i, j):
 	new_path = old_path[0:i] + old_path[j:j+1] + old_path[i+1:j] + old_path[i:i+1] + old_path[j+1:n]
-	new_ans = old_ans - dis(old_path[i-1], old_path[i]) - dis(old_path[i], old_path[(i+1)%n]) - \
-						dis(old_path[j-1], old_path[j]) - dis(old_path[j], old_path[(j+1)%n]) + \
-						dis(new_path[i-1], new_path[i]) + dis(new_path[i], new_path[(i+1)%n]) + \
-						dis(new_path[j-1], new_path[j]) + dis(new_path[j], new_path[(j+1)%n])
-	return new_ans, new_path
+	if random.random() > 0.01:
+		new_ans = old_ans - dis[old_path[i-1]][old_path[i]] - dis[old_path[i]][old_path[(i+1)%n]] - \
+							dis[old_path[j-1]][old_path[j]] - dis[old_path[j]][old_path[(j+1)%n]] + \
+							dis[new_path[i-1]][new_path[i]] + dis[new_path[i]][new_path[(i+1)%n]] + \
+							dis[new_path[j-1]][new_path[j]] + dis[new_path[j]][new_path[(j+1)%n]]
+		return new_ans, new_path
+	else :
+		return dis_cal(new_path), new_path
 
 
-def sa():
-	global n, map
-	n, map = readin.readin()
-	time = 1
-	p = 0.8
-	delta = ((2/(p*n**2))*((time*(10**7))**(1/n)))**(2/n)
+def test_argu(p, delta):
+	count = 1
 	print("p = ", p, "    delta = ", delta)
-	#p = 1; delta = 0.99
+	for i in range(1,n+1):
+		count += count * n*(n-1)/2*p
+		p = p * delta
+	print("p = ", p, "    count = ", count)
+
+
+def sa(input):
+	global n, map
+	n, map = readin.readin(input)
+	inital_dis(n, map)
+	#print(dis)
+	t = 3000
+	delta = 0.999999
 	map = tuple(map)
-	ans_path = [ i for i in range(n) ]
-	#ans_path = [1,14,13,12,7,6,15,5,11,9,10,16,3,2,4,8]
-	#ans_path = [i-1 for i in ans_path]
-	ans_path = tuple(ans_path)
-	ans = dis_cal(ans_path)
-	ready = [(ans, ans_path)]
-	flag = {ans}
-	while len(ready):
-		#print(ready[0])
-		now_ans, now_path = ready[0]
-		del ready[0]
+	now_path = [ i for i in range(n) ]
+	now_path = tuple(now_path)
+	now_ans = dis_cal(now_path)
+	best_ans = now_ans
+	best_path = now_path
+	count = 0
+	count_0 = 0
+	while t > 0.0001:
+		count += 1
+		i = random.randint(0, n - 1)
+		j = random.randint(0, n - 1)
+		while i >= j:
+			i = random.randint(0, n - 1)
+			j = random.randint(0, n - 1)
+		new_ans, new_path= swap(now_ans, now_path, i, j)
+		#print(t, now_ans, now_path)
+		if new_ans < best_ans:
+			best_ans = new_ans
+			best_path = new_path
+		if new_ans < now_ans:
+			now_ans = new_ans
+			now_path = new_path
+		else:
+			e = math.exp((now_ans-new_ans)/t)
+			if random.random() < e:
+				now_ans = new_ans
+				now_path = new_path
+			elif e < 1e-2:
+				count_0 += 1
+			else:
+				count_0 = 0
+			if count_0 == 100:
+				break
+			t *= delta
+			print(t, e)
+	while 1:
+		count += 1
+		print(count)
+		better_ans = best_ans
+		better_path = best_path
 		for i in range(n):
-			for j in range(i+1, n):
-				if i != j:
-					new_ans, new_path= swap(now_ans, now_path, i, j)
-					if new_ans < ans:
-						ans = new_ans
-						ans_path = new_path[:]
-						ready.append((new_ans, new_path))
-						flag.add(new_path)
-						continue
-					if new_ans in flag:
-						continue
-					if random.random() < p:
-						ready.append((new_ans,new_path))
-						flag.add((new_ans, new_path))
-		p *= delta
-	return ans, ans_path
+			for j in range(n):
+				new_ans, new_path = swap(best_ans, best_path, i, j)
+				if new_ans < better_ans:
+					better_ans = new_ans
+					better_path = new_path
+		if better_ans > best_ans:
+			best_ans = better_ans
+			best_path = better_path
+		else:
+			break
+	print("count = ", count)
+	return best_ans, best_path
 
 if __name__ == "__main__":
-	ans, ans_path = sa()
+	ans, ans_path = sa(r"data\ch130.tsp")
+	print("My ans is ", ans, " : ", ans_path)
+	std_path = read_ans.read_ans(n, r"data\ch130.opt.tour")
+	print("Standard ans is ", dis_cal(std_path), " : ", std_path)
 	tmp = [(map[i][0],map[i][1]) for i in ans_path]
 	tmp.append((map[ans_path[0]][0], map[ans_path[0]][1]))
-	ans_path = list(ans_path)
-	for i in range(len(ans_path)):
-		ans_path[i] += 1
-	print("The ans is ", ans, " : ", ans_path)
 	show_path.plot(tmp)
+
+
