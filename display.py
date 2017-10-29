be27e4f5bc9247783f6d878c1b58ca0e8986b44d
Cudd1es <32083795+Cudd1es@users.noreply.github.com>
2017-10-29 11:13:27 +0800

    Revert "display updated"
    This reverts commit f711ae6c73544be4d0c61180b7ecbc1d91e52dd7.

--- display.py
+++ display.py
@@ -49,12 +49,24 @@ class MyDynamicMplCanvas(MyMplCanvas):
     def compute_initial_figure(self):
         self.axes.plot(readin.readin()[1])
 
+<<<<<<< HEAD
     def update_figure(self,opt_tour = std_path.read_std_path(),dif = 1):
         # 构建4个随机整数，位于闭区间[0, 10]
         #l = [random.randint(0, 10) for i in range(4)]
         lf_title = "Deviation degree:" + str(dif)
         self.axes.title(lf_title)
         self.axes.plot(opt_tour)
+=======
+    def update_figure(self):
+        # 构建4个随机整数，位于闭区间[0, 10]
+        l = [random.randint(0, 10) for i in range(4)]
+
+        self.axes.plot([0, 1, 2, 3], l, 'r')
+        #SD = hill_climbing_main.dis_cal(std_path)/best_ans*100
+        #dif = SD
+        #tt = "divention degree:" + str(dif)
+        #self.axes.title(tt)
+>>>>>>> parent of f711ae6... display updated
         self.draw()
 
 class ApplicationWindow(QMainWindow):
@@ -86,14 +98,20 @@ class ApplicationWindow(QMainWindow):
         self.btn1 = QPushButton("SA", self)
         self.btn1.resize(self.btn1.sizeHint())
         #这里改成连接到fgn_2_main.py的连接
+<<<<<<< HEAD
         self.btn1.clicked.connect(SA)
         self.btn1.move(100, 360)
+=======
+        self.btn1.clicked.connect(QCoreApplication.quit)
+        l.addWidget(self.btn1)
+>>>>>>> parent of f711ae6... display updated
 
         #爬山法按钮
         self.btn2 = QPushButton("HC", self)
         self.btn2.resize(self.btn2.sizeHint())
         #这里改成连接到hill_climbing_main.py的连接
         self.btn2.clicked.connect(QCoreApplication.quit)
+<<<<<<< HEAD
         self.btn2.move(350, 360)
 
         #启动按钮
@@ -103,6 +121,17 @@ class ApplicationWindow(QMainWindow):
         self.btn3.clicked.connect(QCoreApplication.quit)
         self.btn3.move(210, 360)
 
+=======
+        l.addWidget(self.btn2)
+
+        #启动按钮
+        self.btn3 = QPushButton("LET'S ROCK ON!!!", self)
+        self.btn3.resize(self.btn3.sizeHint())
+        #这里改成启动程序
+        self.btn3.clicked.connect(QCoreApplication.quit)
+        l.addWidget(self.btn3)
+
+>>>>>>> parent of f711ae6... display updated
         self.main_widget.setFocus()
         self.setCentralWidget(self.main_widget)
 
@@ -118,6 +147,7 @@ class ApplicationWindow(QMainWindow):
     def about(self):
         QMessageBox.about(self, "About", "no about")
 
+<<<<<<< HEAD
 def SA():
 	global n,map
 	n, map = readin.readin(r"data\eil101.tsp")
@@ -179,6 +209,8 @@ def SA():
 	#	iloop = inner_loop
 		k += 1
 		if(t < 0.1): break
+=======
+>>>>>>> parent of f711ae6... display updated
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
